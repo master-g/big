66ee6783caff848b7ea42422a33bac0794f270d3
mg <master-g@users.noreply.github.com>
2018-05-11 19:56:18 +0800

    update

---
+++ foundation_math/ecc/curve/crt.go
@@ -0,0 +1,30 @@
+package curve
+
+// https://rosettacode.org/wiki/Chinese_remainder_theorem#Go
+
+import (
+	"fmt"
+	"math/big"
+)
+
+var (
+	one = big.NewInt(1)
+)
+
+// CRT chinese remainder theorem structures
+func CRT(a, n []*big.Int) (*big.Int, error) {
+	p := new(big.Int).Set(n[0])
+	for _, n1 := range n[1:] {
+		p.Mul(p, n1)
+	}
+	var x, q, s, z big.Int
+	for i, n1 := range n {
+		q.Div(p, n1)
+		z.GCD(nil, &s, n1, &q)
+		if z.Cmp(one) != 0 {
+			return nil, fmt.Errorf("%d not coprime", n1)
+		}
+		x.Add(&x, s.Mul(a[i], s.Mul(&s, &q)))
+	}
+	return x.Mod(&x, p), nil
+}
--- foundation_math/ffcalc/calc/math.go
+++ foundation_math/ffcalc/calc/math.go
@@ -2,6 +2,16 @@ package calc
 
 import "math/big"
 
+var (
+	zero *big.Int
+	one  *big.Int
+)
+
+func init() {
+	zero = big.NewInt(0)
+	one = big.NewInt(1)
+}
+
 // Add return a + b
 func Add(a, b *big.Int) *big.Int {
 	return big.NewInt(0).Add(a, b)
@@ -17,64 +27,36 @@ func Mod(a, b *big.Int) *big.Int {
 	return big.NewInt(0).Mod(a, b)
 }
 
-// ExtendedEuclideanAlgorithm return a * x + b * y = gcd, where gcd is the greatest common divisor of a and b
-func ExtendedEuclideanAlgorithm(a, b *big.Int) (gcd, x, y *big.Int) {
-	s, oldS := big.NewInt(0), big.NewInt(1)
-	t, oldT := big.NewInt(1), big.NewInt(0)
-	r, oldR := big.NewInt(0).Set(b), big.NewInt(0).Set(a)
-	for r.Sign() != 0 {
-		quotient := big.NewInt(0).Div(oldR, r)
-		tempR := big.NewInt(0).Set(quotient).Mul(quotient, r)
-		tempR = tempR.Sub(oldR, tempR)
-		oldR.Set(r)
-		r.Set(tempR)
-
-		tempS := big.NewInt(0).Set(quotient).Mul(quotient, s)
-		tempS = tempS.Sub(oldS, tempS)
-		oldS.Set(s)
-		s.Set(tempS)
-
-		tempT := big.NewInt(0).Set(quotient).Mul(quotient, t)
-		tempT = tempT.Sub(oldT, tempT)
-		oldT.Set(t)
-		t.Set(tempT)
-	}
-
-	return oldR, oldS, oldT
-}
-
 // Mul return op1 * op2
 func Mul(op1, op2 *big.Int) *big.Int {
-	r := big.NewInt(0)
-	r.Mul(op1, op2)
-	return r
+	return big.NewInt(0).Mul(op1, op2)
 }
 
 // Div return op1 / op2
 func Div(order, op1, op2 *big.Int) *big.Int {
-	// a/b = a*b^-1 = a*b^(order-2)
-	r := big.NewInt(0)
-	r.Sub(order, big.NewInt(2))
-	r.Exp(op2, r, nil)
-	r.Mul(op1, r)
-	// r.Mod(r, order)
-	return r
+	invOp2 := Inv(order, op2)
+	return big.NewInt(0).Mul(op1, invOp2)
 }
 
 // Inv return n^-1 mod order
 func Inv(order, n *big.Int) *big.Int {
-	gcd, x, y := ExtendedEuclideanAlgorithm(n, order)
-	tmpNX := big.NewInt(0).Mul(n, x)
-	tmpPY := big.NewInt(0).Mul(order, y)
-	tmpNX.Add(tmpNX, tmpPY)
-	tmpNX.Mod(tmpNX, order)
-	if tmpNX.Cmp(gcd) != 0 {
+	gcd := big.NewInt(0)
+	x := big.NewInt(0)
+	y := big.NewInt(0)
+	nx := big.NewInt(0)
+	py := big.NewInt(0)
+	gcd.GCD(x, y, n, order)
+	nx.Mul(n, x)
+	py.Mul(order, y)
+	nx.Add(nx, py)
+	nx.Mod(nx, order)
+	if nx.Cmp(gcd) != 0 {
 		return nil
 	}
-	if gcd.Cmp(big.NewInt(1)) != 0 {
+	if gcd.Cmp(one) != 0 {
 		return nil
 	}
-	return x.Mod(x, order)
+	return x
 }
 
 // Exp return op1^op2
@@ -84,7 +66,7 @@ func Exp(order, op1, op2 *big.Int) *big.Int {
 		// a^-b = 1/a^b
 		r.Abs(op2)
 		r.Exp(op1, r, nil)
-		return Div(order, big.NewInt(1), r)
+		return Div(order, one, r)
 	} else {
 		r.Exp(op1, op2, nil)
 		return r
